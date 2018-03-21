5f5045ecfd99b5e78b1ae5bdfe0dc777100f5a86
guillaumepn <guillaumepn@free.fr>
2018-03-21 01:52:07 +0100

    fix env files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,11 +9,11 @@ export const environment = {
     ngChatbot: '4b3546820a9b439599fbba9c8ba10abf'
   },
   firebase: {
-    apiKey: "AIzaSyCerGIEnQPPo4WThyynI_6ITe5ZZw2CrRY",
-    authDomain: "chatbot-57089.firebaseapp.com",
-    databaseURL: "https://chatbot-57089.firebaseio.com",
-    projectId: "chatbot-57089",
-    storageBucket: "chatbot-57089.appspot.com",
-    messagingSenderId: "522232257116"
+    apiKey: 'AIzaSyCerGIEnQPPo4WThyynI_6ITe5ZZw2CrRY',
+    authDomain: 'chatbot-57089.firebaseapp.com',
+    databaseURL: 'https://chatbot-57089.firebaseio.com',
+    projectId: 'chatbot-57089',
+    storageBucket: 'chatbot-57089.appspot.com',
+    messagingSenderId: '522232257116'
   }
 };

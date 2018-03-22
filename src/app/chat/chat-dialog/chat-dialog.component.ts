5c3d61045993a45d45b1b0ffc503cab78762f7d8
guillaumepn <guillaumepn@free.fr>
2018-03-22 22:39:17 +0100

    css for home and login

--- src/app/chat/chat-dialog/chat-dialog.component.ts
+++ src/app/chat/chat-dialog/chat-dialog.component.ts
@@ -29,14 +29,15 @@ export class ChatDialogComponent implements OnInit {
 
   ngOnInit() {
     this.firstHTML = `
-    <a target="_blank" href=""><img src="assets/images/sante.png" class="welcome sante"></a> <a target="_blank" href=""><img src="assets/images/localiser.png" class="welcome localiser"></a> <a target="_blank" href="https://www.animalis.com"><img src="assets/images/shopping.png" class="welcome shopping"></a>
+    <a target="_blank" href=""><img src="assets/images/sante.png" class="welcome sante"></a> <a class="localiser-link"><img src="assets/images/localiser.png" class="welcome localiser"></a> <a target="_blank" href="https://www.animalis.com"><img src="assets/images/shopping.png" class="welcome shopping"></a>
     `;
 
     this.data.authenticated.subscribe(authenticated => this.authenticated = authenticated);
     console.log(this.authenticated);
     this.messages = this.chat.conversation.asObservable()
       .scan((acc, val) => acc.concat(val));
-    this.chat.talk();
+
+    // this.chat.talk();
   }
 
   sendMessage() {
@@ -44,6 +45,14 @@ export class ChatDialogComponent implements OnInit {
       this.chat.converse(this.formValue);
     }
     this.formValue = '';
+  }
+
+  findAnimal() {
+    console.log("test");
+    let conversation = document.querySelector('.conversation');
+    let msg = document.createElement('div');
+    msg.innerHTML = `<strong>test</strong>`;
+    conversation.appendChild(msg);
   }
 
 }
